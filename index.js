328c281a84041dba8f26d6d3864b1f21a1871eb2
iqbalhossensorkar <iqbalhossen303@gmail.com>
2023-06-10 22:58:29 +0600

    user added to database successfully

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+app.use(express.json())
+
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.cm8vu8j.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+})
+
+async function run() {
+    try {
+        const usersCollection = client.db('culinaryDB').collection('users')
+
+
+        app.put('/users/:email', async(req, res) => {
+            const user = req.body;
+            const email = req.params.email;
+            const query = {email: email}
+            const options = {upsert: true}
+            const updateDoc = {
+                $set: user
+            }
+            const result = await usersCollection.updateOne(query, updateDoc, options)
+            console.log(result);
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db('admin').command({ ping: 1 })
+        console.log(
+            'Pinged your deployment. You successfully connected to MongoDB!'
+        )
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('culinary Server is running...')
+})
+
+app.listen(port, () => {
+    console.log(`culinary is running on port ${port}`)
+})
