4fff26c204ff9a17753565258a341ff31fc755c2
iqbalhossensorkar <iqbalhossen303@gmail.com>
2023-06-12 05:49:24 +0600

    instructor verify called in user

--- index.js
+++ index.js
@@ -54,13 +54,23 @@ async function run() {
           }
 
 
+        const verifyInstructor = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = {email: email}
+            const user = await usersCollection.findOne(query)
+            if (user?.role !== 'instructor') {
+                return res.status(403).send({ error: true, message: 'forbidden message' })
+            }
+            next();
+        }
+
         app.post('/jwt', (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
             res.send({ token });
         })
 
-        app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
+        app.get('/users', verifyJWT, verifyAdmin, verifyInstructor, async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result);
         })
